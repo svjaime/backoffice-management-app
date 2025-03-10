688f5cac059a096d13734a22587f9ab81b2298e3
Jaime Verde <jaime.verde@gmail.com>
2025-03-10 22:05:27 +0000

    chore: setup i18n with next-intl

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations("HomePage");
+
   return (
     <main>
+      <h1>{t("title")}</h1>
       <Button>Testing Shadcn</Button>
     </main>
   );

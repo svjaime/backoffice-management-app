ba91c6e8231126bedacf5c29cbd17bc1e0042faa
Jaime Verde <jaime.verde@gmail.com>
2025-03-16 08:34:38 +0000

    fix: tooltip formatter

--- frontend/src/components/ui/bar-chart.tsx
+++ frontend/src/components/ui/bar-chart.tsx
@@ -1,3 +1,5 @@
+import CurrencyLabel from "@/components/labels/currency";
+import { useTranslations } from "next-intl";
 import {
   Bar,
   BarChart as RBarChart,
@@ -13,6 +15,8 @@ interface Props {
 }
 
 export default function BarChart({ data, title }: Props) {
+  const t = useTranslations("BarChart");
+
   return (
     <div className="flex w-full flex-col gap-4 rounded-lg border p-4">
       <h3 className="text-lg font-semibold">{title}</h3>
@@ -20,7 +24,12 @@ export default function BarChart({ data, title }: Props) {
         <RBarChart data={data}>
           <XAxis dataKey="label" />
           <YAxis />
-          <Tooltip />
+          <Tooltip
+            formatter={(value: number, _name, _item, index) => [
+              <CurrencyLabel value={value} key={index} />,
+              t("total"),
+            ]}
+          />
           <Bar dataKey="value" fill="#8884d8" />
         </RBarChart>
       </ResponsiveContainer>
